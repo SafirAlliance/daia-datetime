177cec8eae890ed83b8224e444f4a17a19c200e1
Carl Miller Jr. <19718484+halian@users.noreply.github.com>
2025-02-05 23:00:51 -0500

    looks like it works now, knock on wood

--- daiacalendar.py
+++ daiacalendar.py
@@ -1,46 +1,44 @@
 from dataclasses import dataclass
-from datetime import datetime
+from datetime import datetime, timezone
 import time
 
 @dataclass
 class DaiaDateTime:
-    cycle:          int = 0     # currently 35th (so this should be 34 for positive Unix times)
-    yearInCycle:    int = 0     # 1-143
-    yearAbsolute:   int = 0
-    dayAbsolute:    int = 0     # 1-549
-    wheel1Count:    int = 0     # 1-33 (429 days)
-    wheel1Name:     int = 0     # 1-13
-    wheel2Count:    int = 0     # 1-61 (549 days)
-    wheel2Name:     int = 0     # 1-9
-    nata:           int = 0     # 0-15
-    qaren:          int = 0     # 0-71
-    dhagia:         float = 0.0 # 0.0-71.9
+    def __init__(self, secs):
+        self.secs = secs
+        self.years, r = divmod(secs, 39528000)
+        self.cycles = 34 if secs >= 0 else 33
+        self.yearsAbsolute = (self.cycles * 143) + self.years
+        self.days, r = divmod(r, 72000)
+        self.daysAbsolute = (self.years * 549) + self.days
+        self.natai, r = divmod(r, 4500)
+        self.qarenaw, r = divmod(r, 62.5)
+        self.dhagiai = r * 69.12 / 60
+    def __str__(self):
+        gemNames = ['Mau', 'Lòđríř', 'Áradî', 'Nenálû', 'Rŷmňû', 'Řalídam', 'Kànâja', 'Térezî', 'Volánsikhai', 'Kuja', 'Còwì', 'Neital', 'Súvâri']
+        colorNames = ['Sajúmas', 'Cavartûs', 'Âfełaris', 'Mátsunas', 'Basîres', 'Menaós', 'Ìtreus', 'Ávris', 'Îceilis']
+        wheel1Count = d.daysAbsolute % 33
+        wheel1Name = gemNames[int(d.daysAbsolute % 13)]
+        wheel2Count = d.days % 61
+        wheel2Name = colorNames[int(d.days % 9)]
+        wheels = " ".join([str(wheel1Count + 1), wheel1Name, str(wheel2Count + 1), wheel2Name])
+        yearList = " ".join(["year", str(int(d.years + 1)), "of the", ordinal(d.cycles + 1), "cycle"])
+        time = ";".join(["{0:02d}".format(int(d.natai)), "{0:02d}".format(int(d.qarenaw)), "{0:02.1f}".format(d.dhagiai)])
+        return " ".join(["On Daia, today is", wheels, yearList, "and Aran Mean Time is", time])
 
 now = time.time()
 nowReadable = time.ctime(now)
+nowUniversal = datetime.now(timezone.utc)
 
 year = (now / 72000 // 549)
 day = (now // 72000) % 549
 
-d = DaiaDateTime(int(35 if now >= 0 else 34),
-                 int(year),
-                 int(((34 if now >= 0 else 33) * 143) + year),
-                 int(day),
-                 int(day % 33),
-                 int(day % 13),
-                 int(day % 61),
-                 int(day % 9),
-                 int((now // 4500) % 16),
-                 int((now // 62.5) % 72),
-                 (now * 69.12 / 60) % 72)
+d = DaiaDateTime(time.time())
 
-def DaiaPrint(d):
-    print("year:", year)
-    print("day", day)
-    print(d.cycle, d.yearInCycle, d.yearAbsolute, d.dayAbsolute, d.wheel1Count, d.wheel1Name, d.wheel2Count, d.wheel2Name)
-    gemNames = ['Mau', 'Lòđríř', 'Áradî', 'Nenálû', 'Rŷmňû', 'Řalídam', 'Kànâja', 'Térezî', 'Volánsikhai', 'Kuja', 'Còwì', 'Neital', 'Súvâri']
-    colorNames = ['Sajúmas', 'Cavartûs', 'Âfełaris', 'Mátsunas', 'Basîres', 'Menaós', 'Ìtreus', 'Ávris', 'Îceilis']
-    return " ".join(["On Daia, today is", str(d.wheel1Count + 1), gemNames[int(d.wheel1Name)], str(d.wheel2Count + 1), colorNames[int(d.wheel2Name)], "year", str(d.yearInCycle + 1), "of cycle", str(d.cycle), "(", str(d.yearAbsolute + 1), ")", "time", "{0:02d}".format(d.nata), ";", "{0:02d}".format(d.qaren), ";", "{0:02.1f}".format(d.dhagia)])
+def ordinal(n):
+    suffix = {1:'st', 2:'nd', 3:'rd', 11:'th', 12:'th', 13:'th' }
+    return str(n)+(suffix.get(n%100) or suffix.get(n%10,'th'))
 
-print(DaiaPrint(d))
-print("On Earth, today is " + nowReadable)
+print(d)
+print("Your local time: " + nowReadable)
+print("UTC: " + str(nowUniversal))
